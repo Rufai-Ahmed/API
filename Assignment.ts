1b645f2f5ffdc81db6a0f80ef8b37d8193921f10
Rufai-Ahmed <abbeyrufai234@gmail.com>
2023-09-26 12:11:18 +0100

    API

--- Assignment.ts
+++ Assignment.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 import http, { IncomingMessage, ServerResponse } from "http";
 import path from "path";
-import fs from "fs";
+import fs, { existsSync } from "fs";
+import { error } from "console";
 
 interface iMessage {
   message: string;
@@ -30,55 +31,69 @@ const server = http.createServer(
       .on("end", async () => {
         const { method, url } = req;
         //When I hit a route
-        if (method === "GET") {
-          const iniUrl: any = url?.split("/")[1];
-          const usefulUrl = parseInt(iniUrl);
+        // if (method === "GET") {
+        //   const iniUrl: any = url?.split("/")[1];
+        //   const usefulUrl = parseInt(iniUrl);
 
-          const fakeStoreApi = await axios.get(
-            "http://fakestoreapi.com/products"
-          );
+        //   const fakeStoreApi = await axios.get(
+        //     "http://fakestoreapi.com/products"
+        //   );
 
-          if (fakeStoreApi.status) {
-            let fakeStoreData = fakeStoreApi.data;
-            let Data = fakeStoreData.filter((el) => {
-              return el.id === usefulUrl;
-            });
-            status = 200;
-            response.message = "Product details gotten";
-            response.success = true;
-            response.data = Data;
-            res.write(JSON.stringify({ status, response }));
-            res.end();
-          } else {
-            res.write(JSON.stringify({ status, response }));
-            res.end();
-          }
-        } else {
-          response.message = "Wrong Route, try again";
-          response.data = null;
-          response.success = false;
-          res.write(JSON.stringify({ status, response }));
-          res.end();
-        }
+        //   if (fakeStoreApi.status) {
+        //     let fakeStoreData = fakeStoreApi.data;
+        //     let Data = fakeStoreData.filter((el) => {
+        //       return el.id === usefulUrl;
+        //     });
+        //     status = 200;
+        //     response.message = "Product details gotten";
+        //     response.success = true;
+        //     response.data = Data;
+        //     res.write(JSON.stringify({ status, response }));
+        //     res.end();
+        //   } else {
+        //     res.write(JSON.stringify({ status, response }));
+        //     res.end();
+        //   }
+        // } else {
+        //   response.message = "Wrong Route, try again";
+        //   response.data = null;
+        //   response.success = false;
+        //   res.write(JSON.stringify({ status, response }));
+        //   res.end();
+        // }
         //All titles should be saved in a .txt file
         if (method === "GET" && url === "/getTitles") {
           const fakeStoreApi = await axios.get(
             "http://fakestoreapi.com/products"
           );
           if (fakeStoreApi.status) {
-            const fakeStoreDataTitle = fakeStoreApi.data;
-            const TitleFolder = path.join(
-              __dirname,
-              "Titles",
-              `FakeStoreTitles.txt`
+            const fakeStoreDataTitle = fakeStoreApi.data.map((el) => el.title);
+            const TitleFolder = path.join(__dirname, "Titles");
+
+            if (!fs.existsSync) {
+              fs.mkdir(TitleFolder, (error) => error);
+            }
+
+            fs.writeFile(
+              path.join(__dirname, "Titles", "Titles.txt"),
+              fakeStoreDataTitle
+                .toString()
+                .split(",")
+                .flatMap((el) => "\n" + el)
+                .toString(),
+              (error) => {
+                console.log(error);
+              }
             );
 
-            // fakeStoreDataTitle?.pipe(fs.createWriteStream(TitleFolder));
             response.message = "Successful";
             response.success = true;
             response.data = fakeStoreDataTitle;
             status = 200;
             res.write(JSON.stringify({ status, response }));
+            res.end();
+          } else {
+            res.write(JSON.stringify({ response, status }));
             res.end();
           }
         } else {
